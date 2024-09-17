da5b22cbe37d12d5407093b330a1df0330b4610b
Jeppsito <eaa24jepo@students.eaaa.dk>
2024-09-17 15:36:18 +0200

    player og item associeringen + lidt pepsi ogsåen

---
+++ Discord Bot/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot
+{
+    internal class Item
+    {
+
+        public String ItemName { get; set; }
+        public String ItemDescription { get; set; }
+        public double ItemVigor { get; set; }
+        public double ItemEndurance { get; set; }
+        public double ItemStrength { get; set; }
+        public Type ItemType { get; set; }
+      
+        public enum Type
+        {
+            Helmet,
+           BreastPlate,
+           Leggins,
+           Boots,
+           Weapon,
+           Trinket
+            
+        }
+
+        public Item(string itemName, string itemDescription, double itemVigor, double itemEndurance, double itemStrength,Type type)
+        {
+            ItemName = itemName;
+            ItemDescription = itemDescription;
+            ItemVigor = itemVigor;
+            ItemEndurance = itemEndurance;
+            ItemStrength = itemStrength;
+            ItemType = type;
+        }
+    }
+}
